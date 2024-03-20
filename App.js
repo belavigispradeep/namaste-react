6a7b91b5600ce8d0e7da11b70b7a653dedaf6f87
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-21 00:38:44 +0530

    add the parcel

--- App.js
+++ App.js
@@ -1,8 +1,11 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const parent = React.createElement(
   "div",
   { id: "parent" },
   React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "I'm an h1 tag"),
+    React.createElement("h1", {}, "Pradeep"),
     React.createElement("h2", {}, "I'm an h2 tag"),
   ]),
   React.createElement("div",{id:"child2"},[
--- README.md
+++ README.md
@@ -1,1 +1,20 @@
 # Namaste React 🚀
+
+
+# parcel
+- Dev Build
+- Local Server
+- HMR = Hot Module Replacement
+- File Watchimg Algorithm
+- Caching
+- Image Optimization
+- Minification
+- Bundling
+- Compress
+- Consistent Hashing
+- Code Splittung
+- Differntial Bundling - support older browser
+- Diagnostic
+- Error Handling
+- HTTPS
+- Tree Shaking - remove unused file  
