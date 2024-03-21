6fa1d86ed9ddea1b99a75a8da077b7bf530fa8f7
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-21 11:19:45 +0530

    Add the jsx in react

--- App.js
+++ App.js
@@ -1,18 +1,8 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const parent = React.createElement(
-  "div",
-  { id: "parent" },
-  React.createElement("div", { id: "child1" }, [
-    React.createElement("h1", {}, "Pradeep"),
-    React.createElement("h2", {}, "I'm an h2 tag"),
-  ]),
-  React.createElement("div",{id:"child2"},[
-    React.createElement("h1",{},"I'm an h1 tag"),
-    React.createElement("h2",{},"I'm an h2 tag"),
-  ])
-);
+const jsxHeading = <h1 className="heading">Shree Guru Rayaru 🙏</h1>
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+
+root.render(jsxHeading);
