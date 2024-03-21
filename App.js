6eb392f7cb896b913347be276b14a8eb5cfbd217
belavigispradeep <pradeepsbelavigi26@gmail.com>
2024-03-21 23:14:26 +0530

    Add the header

--- App.js
+++ App.js
@@ -1,8 +1,50 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const jsxHeading = <h1 className="heading">Shree Guru Rayaru 🙏</h1>
+const Header = () => {
+  return (
+    <div className="header">
+      <dir className="logo">
+        <img src="foodlogo.png" alt="foodlogo" />
+      </dir>
+      <div className="nav-items">
+        <ul>
+          <li>Home</li>
+          <li>About Us</li>
+          <li>Contact Us</li>
+          <li>Cart</li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+const RestaurantCard = () =>{
+    return(
+        <div className="res-card">
+            <h2>Meghana Foods</h2>
+        </div>
+    )
+}
+
+const Body = () => {
+  return (
+    <div className="body">
+      <div className="search">Search</div>
+      <div className="res-continer">
+        <RestaurantCard/>
+      </div>
+    </div>
+  );
+};
+
+const AppLayout = () => {
+  return (
+    <div className="app">
+      <Header />
+    </div>
+  );
+};
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-
-root.render(jsxHeading);
+root.render(<AppLayout />);
